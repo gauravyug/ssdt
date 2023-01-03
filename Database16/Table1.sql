99a24c2eda704533b93cf6ca624f05fc0f857e1b
Lunatic <34325808+gauravyug@users.noreply.github.com>
2023-01-03 11:03:07 +0530

    Delete Table1.sql

--- Database16/Table1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [Schema1].[Table1]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [name] NCHAR(10) NULL, 
-    [dept] NCHAR(10) NULL, 
-    [manager] NCHAR(10) NOT NULL, 
-    [salary] NCHAR(10) NULL
-)
